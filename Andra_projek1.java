c0bef7283c92ace5fba2eba55c48f72ca786fe80
Andra6-bit <muhammadandrafirdaus@gmail.com>
2025-09-01 09:51:47 +0700

    Add files via upload

---
+++ Andra_projek1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.andra_projek1;
+
+/**
+ *
+ * @author Pongo
+ */
+public class Andra_projek1 {
+
+    public static void main(String[] args) {
+        System.out.println("• Nama    : Muhammad Andra .F");
+        System.out.println("• NPM     : 2440506053");
+        System.out.println("• Alamat  : Sumberarum");
+        System.out.println("• No_HP   : 081246953999");
+    }
+}
